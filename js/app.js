0c4ad93b8644b6a83996fceccc43625df1a43bb4
Michael A. Chapman <chapmanm305@gmail.com>
2019-03-20 11:19:57 -0700

    Create locations array as a reference for object names
    Reformat organizational comments

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 'use strict';
 
-// Creates basic starting table elements
+// ------------------------------------------------------------------------------------
+//
+// Creates table header, body, and footer rows
+//
+// ------------------------------------------------------------------------------------
 function createDataTable() {
   const hoursArr = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
@@ -10,17 +14,20 @@ function createDataTable() {
   table.setAttribute('id', 'table');
   section.appendChild(table);
 
+  let thead = document.createElement('thead');
+  table.appendChild(thead);
+
   let body = document.createElement('tbody');
   body.setAttribute('id', 'tableBody');
   table.appendChild(body);
 
   let tr = document.createElement('tr');
-  body.appendChild(tr);
+  thead.appendChild(tr);
 
   let emptySpace = document.createElement('th');
   tr.appendChild(emptySpace);
 
-  // Create top row filled with hours open
+  // Loop through hoursArr to create headers in thead
   for (let i = 0; i < hoursArr.length; i++) {
     let th = document.createElement('th');
     th.textContent = hoursArr[i];
@@ -43,7 +50,11 @@ function createDataTable() {
   footerRow.appendChild(footerHead);
 }
 
-// Populates individual location rows with cookies per hour as well as location totals
+// ------------------------------------------------------------------------------------
+//
+// Populates individual location table rows with cookies per hour and location totals
+//
+// ------------------------------------------------------------------------------------
 let createData = function() {
   let totalSales = 0;
 
@@ -67,6 +78,13 @@ let createData = function() {
   totals.textContent = totalSales;
   tr.appendChild(totals);
 };
+
+// ------------------------------------------------------------------------------------
+// Locations array for reference
+// constructor function for object creation
+// function calls
+// ------------------------------------------------------------------------------------
+const Locations = ['firstAndPike', 'seaTacAirport', 'seattleCenter', 'capitolHill', 'alki'];
 
 const StoreData = function(name, maxCust, minCust, avgCookies) {
   this.name = name;
